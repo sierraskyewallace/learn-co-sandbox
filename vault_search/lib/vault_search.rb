1d890818df5d1c6351d1b9cd9e75f225811155d6
Sierra Wallace <sskwallace@gmail.com>
2021-07-06 15:37:29 +0000

    Automatically backed up by Learn

--- vault_search/lib/vault_search.rb
+++
@@ -1,6 +0,0 @@
-require "vault_search/version"
-
-module VaultSearch
-  class Error < StandardError; end
-  # Your code goes here...
-end
--- vault_search/lib/vault_search/version.rb
+++
@@ -1,3 +0,0 @@
-module VaultSearch
-  VERSION = "0.1.0"
-end
